f8683f098791c549577a13069640fe1ca1dcb2ba
Marcel Klamm <fujitsustar@hotmail.de>
2025-10-07 22:03:55 +0200

    Add item2 class demonstrating builder pattern for nutrition facts

---
+++ Effective Java/src/chapter2/item2/item2.java
@@ -0,0 +1,222 @@
+package chapter2.item2;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Item2: Consider a builder when faced with many constructor parameters
+ */
+public class item2 {
+    NutritionFactsTelescoping cocaCola = new NutritionFactsTelescoping(240, 8, 100, 0, 35, 27);
+    NutritionFactsJavaBeans cocaCola2 = new NutritionFactsJavaBeans();
+    {
+        cocaCola2.setServingSize(240);
+        cocaCola2.setServings(8);
+        cocaCola2.setCalories(100);
+        cocaCola2.setSodium(35);
+        cocaCola2.setCarbohydrate(27);
+    }
+    NutritionFactsBuilder cocaCola3 = new NutritionFactsBuilder.Builder(240, 8).calories(100).sodium(35)
+            .carbohydrate(27).build();
+}
+
+/**
+ * Telescoping constructor pattern - does not scale well
+ */
+class NutritionFactsTelescoping {
+    private final int servingSize; // (mL) required
+    private final int servings; // (per container) required
+    private final int calories; // (per serving) optional
+    private final int fat; // (g) optional
+    private final int sodium; // (mg) optional
+    private final int carbohydrate; // (g) optional
+
+    public NutritionFactsTelescoping(int servingSize, int servings) {
+        this(servingSize, servings, 0);
+    }
+
+    public NutritionFactsTelescoping(int servingSize, int servings, int calories) {
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFactsTelescoping(int servingSize, int servings, int calories, int fat) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFactsTelescoping(int servingSize, int servings, int calories, int fat, int sodium) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+
+    public NutritionFactsTelescoping(int servingSize, int servings, int calories, int fat, int sodium,
+            int carbohydrate) {
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+}
+
+/**
+ * JavaBeans pattern - allows inconsistency, mandates mutability
+ */
+class NutritionFactsJavaBeans {
+    private int servingSize = -1; // required; no default value
+    private int servings = -1; // required; no default value
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public NutritionFactsJavaBeans() {
+    }
+
+    // Setters
+    public void setServingSize(int servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public void setSodium(int sodium) {
+        this.sodium = sodium;
+    }
+
+    public void setCarbohydrate(int carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+}
+
+/**
+ * Builder Pattern
+ */
+class NutritionFactsBuilder {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters - initialized to default values
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val) {
+            carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFactsBuilder build() {
+            return new NutritionFactsBuilder(this);
+        }
+    }
+
+    private NutritionFactsBuilder(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+}
+
+abstract class Pizza {
+
+    public enum Topping {
+        HAM, MUSHROOM, ONION, PEPPER, SAUSAGE
+    }
+
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        abstract Pizza build();
+
+        // Subclasses must override this method to return "this"
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone(); // See Item 50
+    }
+}
+
+class NyPizza extends Pizza {
+    public enum Size {
+        SMALL, MEDIUM, LARGE
+    }
+
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private final Size size;
+
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        public NyPizza build() {
+            return new NyPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+    }
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+
+}
